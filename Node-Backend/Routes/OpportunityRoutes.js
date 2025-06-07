0531e2531f8956f616cb01455d7dcf98f5d9aae3
mrjayrai <raijai127@gmail.com>
2025-06-07 23:57:18 +0530

    Integrated Opportunity API node and flask

--- Node-Backend/Routes/OpportunityRoutes.js
+++ Node-Backend/Routes/OpportunityRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const {addOpportunity} = require('../controllers/Addopportunity');
 
 router.post('/add-opportunity', addOpportunity);
+router.post('/get-matched-opportunity', require('../controllers/GetMatchedOpportunity'));
 
 module.exports = router;
