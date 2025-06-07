d4d139e980f4cf28d1e1787e767a26aba0066337
mrjayrai <raijai127@gmail.com>
2025-06-07 13:20:11 +0530

    Changed Port to 4000

--- Node-Backend/index.js
+++ Node-Backend/index.js
@@ -17,7 +17,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
