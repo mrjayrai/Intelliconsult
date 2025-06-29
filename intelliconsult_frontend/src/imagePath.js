c448f392d487f230cf5068a3ef536ee4e2a8066a
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2025-06-30 00:30:55 +0530

    Update imagePath.js

--- intelliconsult_frontend/src/imagePath.js
+++ intelliconsult_frontend/src/imagePath.js
@@ -1,4 +1,4 @@
 // const imagepath = "http://localhost:4000/";
- const imagepath = "http://node-backend-3n5w.onrender.com/";
+ const imagepath = "https://node-backend-3n5w.onrender.com/";
 
 export default imagepath;
