9a98bf870320de874fac8323ad62570ebc971ce0
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2025-06-30 00:31:06 +0530

    Update apiLink.js

--- intelliconsult_frontend/src/apiLink.js
+++ intelliconsult_frontend/src/apiLink.js
@@ -1,4 +1,4 @@
 // const api = "http://localhost:4000/api/";
- const api = "http://node-backend-3n5w.onrender.com/api/";
+ const api = "https://node-backend-3n5w.onrender.com/api/";
 
 export default api;
