c60875ea05afbddedb995e873b7feeb76407d89c
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2025-06-30 00:10:13 +0530

    Update apiLink.js

--- intelliconsult_frontend/src/apiLink.js
+++ intelliconsult_frontend/src/apiLink.js
@@ -1,4 +1,4 @@
-const api = "http://localhost:4000/api/";
-// const api = "http://34.203.204.195:4000/api/";
+// const api = "http://localhost:4000/api/";
+ const api = "http://100.24.42.227:4000/api/";
 
 export default api;
