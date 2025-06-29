853b496eacd4fc9848caa45a5fa66bc22698c048
Rai Pritesh <84077582+mrjayrai@users.noreply.github.com>
2025-06-30 00:10:44 +0530

    Update imagePath.js

--- intelliconsult_frontend/src/imagePath.js
+++ intelliconsult_frontend/src/imagePath.js
@@ -1,4 +1,4 @@
-const imagepath = "http://localhost:4000/";
-// const imagepath = "http://34.203.204.195:4000/";
+// const imagepath = "http://localhost:4000/";
+ const imagepath = "http://100.24.42.227:4000/";
 
 export default imagepath;
